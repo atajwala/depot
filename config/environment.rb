3ad7c53d53797e0f20bdbb444c1caa9a3f37ef5b
Aslam Tajwala <atajwala@gmail.com>
2011-06-13 18:41:50 -0700

    Implemented checkout feature + email communication

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+# Global config for mail
+Depot::Application.configure do
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+		:address => "smtp.gmail.com",
+		:port => 587,
+		:domain => "domain.of.sender.net",
+		:authentication => "plain",
+		:user_name => "aslam",
+		:password => "secret",
+		:enable_starttls_auto => true
+	}
+end
