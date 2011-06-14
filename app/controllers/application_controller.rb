1664c007352696529f575cb601a78e2db7739d89
Aslam Tajwala <atajwala@gmail.com>
2011-06-13 20:56:32 -0700

    log-in/out functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	before_filter :authorize
   protect_from_forgery
 
 	private
@@ -11,4 +12,11 @@ class ApplicationController < ActionController::Base
 			cart
 		end
 	
+	protected
+		def authorize
+			unless User.find_by_id(session[:user_id])
+				redirect_to login_url, :notice => "Please log in"
+			end
+		end
+		
 end
